2ca1a7c07d769c4777b185aaa50c74bafcd3892b
dcampos08 <dcampos@civisanalytics.com>
2016-12-20 10:21:15 -0500

    Update CivisDemo.R

--- CivisDemo.R
+++ CivisDemo.R
@@ -1,3 +1,27 @@
-library(civis.r.client)
+# Civis API calls with the Civis R Client
 
-databases_list()
+# Need to be anonymized!
+
+library( civis.r.client )
+
+# Example of reading in a table from Redshift
+tpc <- read_civis( 
+  database='Civis Database',
+  tablename='retail.customers'
+)
+
+# Example of querying from Redshift
+tpc.example <- read_civis(
+  database='Civis Database',
+  sql=paste( 'select * from retail.customers limit 50' )
+)
+
+# Example of writing to Readshift
+write_civis(
+  df=tpc.example,
+  tablename='retail.customers_limited',
+  database='Civis Database',
+  distkey='person_id',
+  sortkey1='person_id'
+)
+

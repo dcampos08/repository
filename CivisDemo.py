ff4d66cade76ec3694dfc3d7fa166022f082f5fe
dcampos08 <dcampos@civisanalytics.com>
2017-02-14 09:34:00 -0500

    Update CivisDemo.py

--- CivisDemo.py
+++ CivisDemo.py
@@ -1,8 +1,12 @@
 import civis
-
 client = civis.APIClient()
 
-my_tables = client.tables.list(database_id = 326, schema = 'mailing')
+import pandas as pd
 
-for tt in my_tables:
-    print(tt['name'])
+my_table = civis.io.read_civis_sql(sql="select* from geocoding.housing_units limit 10",database="FEMA",use_pandas=True)
+
+final_dataframe = pd.DataFrame(my_table, columns = ['housing_unit_id','state_code'])
+
+civis.io.query_civis(sql='drop table if exists public.dewberry_training', database='FEMA')
+
+civis.io.dataframe_to_civis(df=final_dataframe, database='FEMA', table='public.dewberry_training')
